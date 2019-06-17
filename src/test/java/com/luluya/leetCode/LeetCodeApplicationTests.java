f74764eba7dc15bcb324bcac0a9537ac1c1d88d6
chenjialu <chenjialu0804@163.com>
2019-06-17 10:30:20 +0800

    init

---
+++ src/test/java/com/luluya/leetCode/LeetCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luluya.leetCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
